324925baa5b35a0bbd9d9d7bcfa555fa23ae8a9b
adirelm <adir4455@gmail.com>
2022-10-22 03:56:26 +0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { sequelize } from "./helpers/database";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+
 import serverHandler from "./routes/server";
+
+const PORT = process.env.PORT ?? 3000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Sample server",
+      version: "1.0.0",
+      description: "Sample servers on the web",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: [__dirname + "/routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
 
 const app = express();
 
 app.use(bodyParser.json());
 
 // Routes middleware
+app.use("/api", swaggerUI.serve, swaggerUI.setup(specs));
 app.use(serverHandler);
 
-app.listen({ port: 3000 }, async () => {
+app.listen({ port: PORT }, async () => {
   try {
     // await sequelize.sync({ force: true });
     await sequelize.authenticate();
--- src/routes/server.ts
+++ src/routes/server.ts
@@ -4,6 +4,56 @@ import Server from "../models/server";
 
 const serverHandler = Router();
 
+/**
+ * @swagger
+ * components:
+ *
+ *  schemas:
+ *    Server:
+ *      type: object
+ *      required:
+ *        - name
+ *        - url
+ *      properties:
+ *        id:
+ *          type: number
+ *          description: The id of the server
+ *        name:
+ *          type: string
+ *          description: The name of the server
+ *        url:
+ *          type: string
+ *          description: The url of the server
+ *
+ *
+ *  parameters:
+ *    ServerIdPathParam:
+ *      name: serverId
+ *      in: path
+ *      description: Server id
+ *      required: true
+ *      schema:
+ *        type: number
+ */
+
+/**
+ * @swagger
+ * /servers:
+ *  get:
+ *    tags:
+ *      - Server
+ *    summary: Returns the list of all the servers
+ *    responses:
+ *      200:
+ *        description: The list of the servers
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Server'
+ */
+
 serverHandler.get("/servers", async (req, res, next) => {
   try {
     const servers = await Server.findAll();
@@ -18,6 +68,30 @@ serverHandler.get("/servers", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /server:
+ *  post:
+ *    tags:
+ *      - Server
+ *    summary: Returns the list of all the servers
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/Server"
+ *    responses:
+ *      201:
+ *        description: Server Created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Server'
+ */
+
 serverHandler.post("/server", async (req, res, next) => {
   const name = req.body.name;
   const url = req.body.url;
@@ -27,27 +101,88 @@ serverHandler.post("/server", async (req, res, next) => {
 
     res
       .status(201)
-      .json({ status: 200, message: "Server created", data: server });
+      .json({ status: 201, message: "Server created", data: server });
   } catch (error) {
     console.log(error);
   }
 });
 
-serverHandler.patch("/server/:id", async (req, res, next) => {
-  const id = req.params.id;
+/**
+ * @swagger
+ * /server/{serverId}:
+ *  patch:
+ *    tags:
+ *      - Server
+ *    summary: Update server by id
+ *    parameters:
+ *      - $ref: "#/components/parameters/ServerIdPathParam"
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/Server"
+ *    responses:
+ *      200:
+ *        description: Server updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Server'
+ *      404:
+ *        description: Not found
+ */
+
+serverHandler.patch("/server/:serverId", async (req, res, next) => {
+  const id = req.params.serverId;
+  const name = req.body.name;
+  const url = req.body.url;
 
   try {
     const server = await Server.findByPk(id);
+
+    if (server) {
+      const updatedServer = await server.update({ name, url });
+      res
+        .status(200)
+        .json({ status: 200, message: "Server updated", data: updatedServer });
+    }
+
     res
-      .status(200)
-      .json({ status: 200, message: "Server updated", data: server });
+      .status(404)
+      .json({ status: 404, message: "Server not found", data: [] });
   } catch (error) {
     console.log(error);
   }
 });
 
-serverHandler.delete("/server/:id", async (req, res, next) => {
-  const id = req.params.id;
+/**
+ * @swagger
+ * /server/{serverId}:
+ *  delete:
+ *    tags:
+ *      - Server
+ *    summary: Delete server by id
+ *    parameters:
+ *      - $ref: "#/components/parameters/ServerIdPathParam"
+ *
+ *
+ *    responses:
+ *      200:
+ *        description: Server deleted
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Server'
+ *      404:
+ *        description: Server not found
+ */
+
+serverHandler.delete("/server/:serverId", async (req, res, next) => {
+  const id = req.params.serverId;
 
   try {
     const server = await Server.findByPk(id);
