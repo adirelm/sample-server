e0386e0458d585d9a519d50055a63bec416c3666
adirelm <adir4455@gmail.com>
2022-10-22 16:47:51 +0300

    database modify temp

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -10,8 +10,18 @@ const userName = process.env.USER_NAME!;
 const password = process.env.PASSWORD!;
 const host = process.env.HOST!;
 
-export const sequelize = new Sequelize(serverName, userName, password, {
-  host: host,
-  dialect: "postgres",
-  models: [Server, History],
-});
+// export const sequelize = new Sequelize(serverName, userName, password, {
+//   host: host,
+//   dialect: "postgres",
+//   models: [Server, History],
+// });
+export const sequelize = new Sequelize(
+  "sample-server-adir",
+  "zmtpbrnahytmdy",
+  "1cdbc716d6574fc611b99b4bcf1daae38b32d0ada6c57b8b4cdd85663f6f5824",
+  {
+    host: "ec2-23-20-140-229.compute-1.amazonaws.com",
+    dialect: "postgres",
+    models: [Server, History],
+  }
+);
