03a49218847b62184b88a9d8a34d89748722025f
adirelm <adir4455@gmail.com>
2022-10-24 21:13:50 +0300

    update welcome mail

--- dist/helpers/email.js
+++ dist/helpers/email.js
@@ -103,7 +103,9 @@ const userWelcomeEmailTemplate = function (username, firstname) {
 </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="a67573db-d29e-4372-8c86-43b9f4d3d576" data-mc-module-version="2019-10-22">
 <tbody>
   <tr>
-    <td style="padding:18px 0px 18px 0px; line-height:16px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: center"><strong>Enter a server url for sampling, and enjoy a server health check with email notifications!</strong></div><div></div></div></td>
+    <td style="padding:18px 0px 18px 0px; line-height:16px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: center"><strong>Your user is currently in </strong><span style="color: #e50808"><strong>pending</strong></span><strong> status, soon you will be approved and you can enter a server url for sampling, and enjoy a health check of the server with email notifications!<br>
+<br>
+</strong><span style="color: #c6b8b8"><strong>We will notify you when your user is activated.</strong></span></div><div></div></div></td>
   </tr>
 </tbody>
 </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"></div><p style="font-size:12px; line-height:20px;"><a class="Unsubscribe--unsubscribeLink" href="{{{unsubscribe}}}" target="_blank" style="">Unsubscribe</a> - <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="">Unsubscribe Preferences</a></p></div></td>
